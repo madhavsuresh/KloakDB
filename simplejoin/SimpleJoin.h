53b5b7af766bd771fa2852da7f6ef656c20b3dd1
madhav <madhav@hitman.cs.northwestern.edu>
2018-06-06 15:29:16 -0500

    cleaning up files

--- simplejoin/SimpleJoin.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#ifndef SimpleJoin 
-#define SimpleJoin 
-
-vector< vector<int> > simplejoin(vector< vector<int> > table1, vector< vector<int> > table2, int join_column1_id, int join_column2_id);
-
-
-#endif
