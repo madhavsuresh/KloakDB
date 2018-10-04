ef90ee1da337f8c902c5e3aaa7bd637a83737ef6
Madhav Suresh <madhav@thoughtbison.net>
2018-10-04 13:34:00 -0500

    adding HonestBrokerClient.cpp/HonestBrokerClient.h

---
+++ rpc/HonestBrokerClient.cpp
@@ -0,0 +1,38 @@
+//
+// Created by madhav on 10/1/18.
+//
+
+#include "HonestBrokerClient.h"
+#include "vaultdb.grpc.pb.h"
+#include "vaultdb.pb.h"
+
+int HonestBrokerClient::GetNumHosts() {
+    vaultdb::NumHostsRequest req;
+    vaultdb::NumHostResp resp;
+    grpc::ClientContext context;
+
+    auto status = stub_->NumHosts(&context, req, &resp);
+
+    if (status.ok()) {
+        return resp.num_hosts();
+    } else {
+        std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
+        throw;
+    }
+}
+
+int HonestBrokerClient::Register(std::string hostname) {
+    vaultdb::RegisterRequest req;
+    vaultdb::RegisterResponse resp;
+    grpc::ClientContext context;
+
+    req.set_hostname(hostname);
+
+    auto status = stub_->Register(&context, req, &resp);
+    if (status.ok()) {
+        return resp.host_num();
+    } else {
+        std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
+        throw;
+    }
+}
---
+++ rpc/HonestBrokerClient.h
@@ -0,0 +1,24 @@
+//
+// Created by madhav on 10/1/18.
+//
+
+#ifndef PROJECT_HONESTBROKERCLIENT_H
+#define PROJECT_HONESTBROKERCLIENT_H
+
+
+#include "vaultdb.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
+
+class HonestBrokerClient {
+public:
+    HonestBrokerClient(std::shared_ptr<grpc::Channel> channel)
+    : stub_(vaultdb::HonestBroker::NewStub(channel)) {}
+
+    int GetNumHosts();
+    int Register(std::string hostname);
+
+private:
+    std::unique_ptr<vaultdb::HonestBroker::Stub> stub_;
+};
+
+#endif //PROJECT_HONESTBROKERCLIENT_H
