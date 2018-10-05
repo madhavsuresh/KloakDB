bf5bb0713307e0ca4f37c6a568f97409833d57d8
root <root@dhcp181207.cs.northwestern.edu>
2018-10-05 14:31:51 -0500

    switched to will branch and started hash join

---
+++ HashJoin.h
@@ -0,0 +1,12 @@
+//
+// Created by root on 10/4/18.
+//
+#include "vaultdb_generated.h"
+#include "postgres_client.h"
+
+
+#ifndef PROJECT_HASHJOIN_H
+#define PROJECT_HASHJOIN_H
+
+#endif //PROJECT_HASHJOIN_H
+table_t *HashJoin(table_t * left_table, table_t * right_table, uint16_t l_col, uint16_t r_col);
