5c2a525187e34e084f80b90da68e25b248d9def3
Madhav Suresh <madhav@thoughtbison.net>
2018-10-25 15:23:00 -0500

    finished aggregate

---
+++ aggregate_test.cpp
@@ -0,0 +1,40 @@
+//
+// Created by madhav on 10/25/18.
+//
+#include <gtest/gtest.h>
+#include "postgres_client.h"
+#include "Aggregate.h"
+
+class aggregate_test : public ::testing::Test {
+public:
+    std::string dbname;
+protected:
+    void SetUp() override {
+      dbname = "dbname=test";
+      std::string query1("create table simple_agg (a INT, b INT)");
+      query(query1, dbname);
+    };
+    void TearDown() override {
+      std::string query1("DROP TABLE simple_agg");
+      query(query1, dbname);
+    };
+};
+
+TEST_F(aggregate_test, simple_aggregate) {
+  std::string query1 = "INSERT INTO simple_agg (a,b) VALUES (1,7), (2,7), (3,4)";
+  query(query1, dbname);
+  query1 = "SELECT * FROM simple_agg";
+  table_t *t = get_table(query1, dbname);
+  groupby_def_t gbd;
+  gbd.type = COUNT;
+  gbd.colno = 1;
+  table_t *t2 = aggregate(t, &gbd);
+  ASSERT_EQ(2, t2->num_tuples);
+  tuple_t * tup = get_tuple(0, t2);
+  ASSERT_EQ(tup->field_list[0].f.int_field.val, 4);
+  ASSERT_EQ(tup->field_list[1].f.int_field.val, 1);
+
+  tup = get_tuple(1, t2);
+  ASSERT_EQ(tup->field_list[0].f.int_field.val, 7);
+  ASSERT_EQ(tup->field_list[1].f.int_field.val, 2);
+}
