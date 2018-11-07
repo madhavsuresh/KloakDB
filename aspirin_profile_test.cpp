b617b1d80c85f56252ac2cd518c1bb26fdf7756a
Madhav Suresh <madhav@thoughtbison.net>
2018-11-07 15:17:46 -0600

    benchmarking and segfault fix, and memory leak fix

--- aspirin_profile_test.cpp
+++ aspirin_profile_test.cpp
@@ -2,9 +2,11 @@
 // Created by madhav on 11/6/18.
 //
 #include <gtest/gtest.h>
+#include <chrono>
 #include "pqxx_compat.h"
 #include "HashJoin.h"
 #include "Aggregate.h"
+#include "Logger.h"
 
 class aspirin_profile_test :public ::testing::Test {
 public:
@@ -34,6 +36,7 @@ protected:
       table_t * demographics_scan = get_table("SELECT * from demographics", dbname);
       table_t * diagnoses_scan = get_table("SELECT * from diagnoses where diag_src = 'hd'", dbname);
       table_t * vitals_scan = get_table("SELECT * from vitals", dbname);
+      auto start = std::chrono::high_resolution_clock::now();
 
       join_def_t vd_join;
       vd_join.l_col = colno_from_name(vitals_scan, "patient_id");
@@ -56,8 +59,8 @@ protected:
       table_t * vitals_diagnosis_join_medication_join = hash_join(vitals_diagnosis_join, medications_scan, vdijm_join);
 
       join_def_t vdijmjde_join;
-      vdijm_join.l_col = colno_from_name(vitals_diagnosis_join_medication_join, "patient_id");
-      vdijm_join.r_col = colno_from_name(demographics_scan, "patient_id");
+      vdijmjde_join.l_col = colno_from_name(vitals_diagnosis_join_medication_join, "patient_id");
+      vdijmjde_join.r_col = colno_from_name(demographics_scan, "patient_id");
       vdijmjde_join.project_len = 4;
       vdijmjde_join.project_list[0].side  = LEFT_RELATION;
       vdijmjde_join.project_list[0].col_no = colno_from_name(vitals_diagnosis_join_medication_join, "patient_id");
@@ -76,6 +79,24 @@ protected:
       gbd.gb_colnos[0] = colno_from_name(vitals_diagnosis_join_medication_join_demographics_join, "gender");
       gbd.gb_colnos[1] = colno_from_name(vitals_diagnosis_join_medication_join_demographics_join, "race");
       table_t * final_avg = aggregate(vitals_diagnosis_join_medication_join_demographics_join,&gbd);
+      auto finish = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> elapsed = finish - start;
+      std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+
+
+  printf("\n");
+      for (int i = 0; i < final_avg->num_tuples; i++) {
+          print_tuple(get_tuple(i, final_avg));
+        printf("\n");
+      }
+        free_table(medications_scan);
+        free_table(demographics_scan);
+        free_table(diagnoses_scan);
+        free_table(vitals_scan);
+        free_table(vitals_diagnosis_join);
+        free_table(vitals_diagnosis_join_medication_join);
+        free_table(vitals_diagnosis_join_medication_join_demographics_join) ;
+        free_table(final_avg);
     }
 
 
