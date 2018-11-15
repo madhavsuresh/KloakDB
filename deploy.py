1cf996a09ae659b385e72371d96c836a9bd424f5
root <root@dhcp181207.cs.northwestern.edu>
2018-11-15 17:55:46 -0600

    most of way through deployment

--- deploy.py
+++ deploy.py
@@ -3,7 +3,9 @@ import paramiko
 from subprocess import call
 import base64
 import json
+import time
 import threading
+import sys
 ### HELPER FUNCTIONS ###
 
 # TODO: test this on other machines.
@@ -12,53 +14,62 @@ import threading
 # TODO: confirm that text file busy is because using localhost... ?
 # custom sink for teh logger write back rpc calls to honest broker.
 
-def copy_vaultdb(user,address):
-    print("Copying vaultdb executable over to host address %s" %(address))
-    user_addr_str = user + "@" + address + ":~" # scp to home dir
-    call(['scp', '-i', '~/.ssh/id_rsa','vaultdb', user_addr_str])
-    return
-
-def get_and_build_vaultdb(user,address, repo_addr,ssh):
+def get_and_build_vaultdb(user,address, repo_addr,ssh, install_dir):
     print("Downloading from Github vaultdb on address %s" %(address))
-    user_addr_str = user + "@" + address
-    command = "git clone " + repo_addr
-    run_command(user_addr_str, command)
+    command = "git clone " + repo_addr + " " + install_dir + "/vaultdb_operators" # make new folder for vaultdb
+    i,out,err = run_command_blocking(command,ssh)
+    print(err.readlines())
+    print("Downloaded Github Repo")
 
-    command = "cd vaultdb_operators"
-    run_command(user,address, command)
+    command = "cd " + install_dir +  "/vaultdb_operators"
+    i,out,err = run_command_blocking(command,ssh)
+    print(err.readlines())
 
-    command = "./configure.sh"
-    run_command(user,address, command)
+    print("configuring")
+    command = install_dir + "/vaultdb_operators/configure.sh"
+    i,out,err = run_command_blocking(command,ssh)
+    print(err.readlines())
+    print("done configuring")
+
     return
+
+def run_command_blocking(command,ssh):
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
+    ssh_stdout.channel.recv_exit_status() # important line, this function won't return until honest broker is done...
+
+    return ssh_stdin, ssh_stdout, ssh_stderr
+
 
 def run_command(user,address, command,ssh):
 
-    ssh.connect(hostname = address, username=user, key_filename='/root/.ssh/id_rsa') # must have keys setup
     ssh_stdin,ssh_stdout,ssh_stderr = ssh.exec_command(command)
 
-    return
+    return ssh_stdin,ssh_stdout,ssh_stderr
 
 def kill_vaultdb(user,address,ssh):
     command = "killall -9 vaultdb"
     run_command(user,address,command,ssh)
-
-def start_vaultdb_host(user,address, port, hb_info, ssh,experiment_number):
-
-    print("Connecting to %s to start vault db"% (address))
-    addr_port = address + ":" + port
-    command = "./vaultdb " + "-honest_broker_address=" + hb_info + " -address=" + addr_port + " -experiment=" + experiment_number
-    ssh_stdin,ssh_stdout,ssh_stderr = ssh.exec_command(command)
-    # check the return??
     return
 
 
-def start_vaultdb_hb(user,address, port, ssh, experiment_number):
-    print("Connecting to %s with user %s to start vault db"% (address, user))
+def start_vaultdb_host(user,address, port, hb_info, ssh, experiment_number, base_dir):
 
+    print("Connecting to %s to start vault db"% (address))
     addr_port = address + ":" + port
-    command = "./vaultdb " + "-honest_broker=true " + " -address=" + addr_port + " -experiment=" + experiment_number
+    command = "LD_LIBRARY_PATH=~/vaultdb_operators/lib/lib " + base_dir + "vaultdb_operators/build/vaultdb " + "-honest_broker_address=" + hb_info + " -address=" + addr_port #+ " -experiment=" + experiment_number
+    ssh_stdin,ssh_stdout,ssh_stderr = run_command(user,address,command,ssh)
+    # check the return?
+    return ssh_stdin,ssh_stdout,ssh_stderr
 
-    ssh_stdin,ssh_stdout,ssh_stderr = ssh.exec_command(command)
+
+def start_vaultdb_hb(user,address, port, ssh, experiment_number, num_hosts, base_dir):
+    print("Connecting to %s with user %s to start vault db"% (address, user))
+    print("THREAD!")
+
+    addr_port = address + ":" + port                                    # assuming that we build it in vaultdb_operators and configure is run
+    command = "LD_LIBRARY_PATH=~/vaultdb_operators/lib/lib " + base_dir +  "vaultdb_operators/build/vaultdb " + "-honest_broker=true " + " -address=" + addr_port #+ " -experiment=" + experiment_number + " -expected_num_hosts=" + num_hosts
+
+    ssh_stdin,ssh_stdout,ssh_stderr = run_command(user,address,command,ssh)
 
     ssh_stdout.channel.recv_exit_status() # important line, this function won't return until honest broker is done...
 
@@ -69,42 +80,69 @@ def start_vaultdb_hb(user,address, port, ssh, experiment_number):
 
 BASE_DIR = os.getcwd()
 
-ssh = paramiko.SSHClient()
-ssh.load_system_host_keys() # loads current ssh authorized hosts/keys
-ssh.set_missing_host_key_policy(paramiko.WarningPolicy) # if not found add
-# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+def new_ssh():
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys() # loads current ssh authorized hosts/keys
+    ssh.set_missing_host_key_policy(paramiko.WarningPolicy) # if not found add
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    return ssh
+
+ssh_queue = []
 
 
 with open(BASE_DIR + "/deploy.json") as json_data:
     data = json.load(json_data)
-
+    host_list = data["Hosts"]
+    experiments = data["Experiments"]
+    github_repo = data["Github"]
+    base_dir = data["BASE_DIR"]
     # download and compile vaultdb on every machine
     hb_addr_port = data["HB"]["address"] + ":" + data["HB"]["port"]
- #   get_and_build_vaultdb(data["HB"]["user"], data["HB"]["address"])
 
-    host_list = data["Hosts"]
+    # for all hosts make ssh client
     for host in host_list:
-  #      get_and_build_vaultdb(host["user"], host["address"],ssh)
-	command = "LD_LIBRARY_PATH=~/vaultdb_operators/lib/lib"
-	run_command(host["user"],host["address"], command,ssh)
-	command = "export LD_LIBRARY_PATH"
-	run_command(host["user"],host["address"], command,ssh)
+        new_ssh = paramiko.SSHClient()
+        new_ssh.load_system_host_keys() # loads current ssh authorized hosts/keys
+        new_ssh.set_missing_host_key_policy(paramiko.WarningPolicy) # if not found add
+        ssh_queue.append(new_ssh)
+        new_ssh.connect(hostname = host["address"], username=host["user"]) # must have keys setup
+
+        get_and_build_vaultdb(host["user"], host["address"],github_repo,new_ssh, base_dir)
 
 
-    experiments = data["Experiments"]
+    sys.exit(0)
+
     for experiment in experiments:
         #make a thread to wait to until the experiment is finished
-        tid = threading.Thread(target=start_vaultdb_hb,args=(data["HB"]["user"], data["HB"]["address"],data["HB"]["port"], experiment ))
 
+        ssh_hb = paramiko.SSHClient()
+        ssh_hb.load_system_host_keys() # loads current ssh authorized hosts/keys
+        ssh_hb.set_missing_host_key_policy(paramiko.WarningPolicy) # if not found add
+
+        num_hosts = len(host_list)
+
+        tid = threading.Thread(target=start_vaultdb_hb,args=(data["HB"]["user"], data["HB"]["address"],data["HB"]["port"], ssh_hb, experiment,num_hosts, base_dir ))
+        tid.start()
+        time.sleep(2)
         for host in host_list:
-            start_vaultdb_host(host["user"], host["address"], host["port"], hb_addr_port, ssh, experiment)
+            new_ssh = paramiko.SSHClient()
+            new_ssh.load_system_host_keys() # loads current ssh authorized hosts/keys
+            new_ssh.set_missing_host_key_policy(paramiko.WarningPolicy) # if not found add
+            ssh_queue.append(new_ssh)
+            a,b,c = start_vaultdb_host(host["user"], host["address"], host["port"], hb_addr_port, new_ssh, experiment, base_dir)
+
+
+
 
         while(tid.isAlive()): # wait for the hb to return and finish
-            print("waiting on hb")
+            #print("waiting on hb")
             continue
 
-        for host in host_list:
-            kill_vaultdb(host["user"],host["address"],ssh)
 
-    # start_vaultdb_hb(data["HB"]["user"], data["HB"]["address"],data["HB"]["port"])
+        for host in host_list: # call killall (probably only need to do it once)
+            new_ssh = paramiko.SSHClient()
+            new_ssh.load_system_host_keys() # loads current ssh authorized hosts/keys
+            new_ssh.set_missing_host_key_policy(paramiko.WarningPolicy) # if not found add
+            run_command(host["user"],host["address"], "killall -9 vaultdb", new_ssh)
+
 
