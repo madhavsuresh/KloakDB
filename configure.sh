f542b58ddeee1cc053ce86dc7edafa11e47deaab
Madhav Suresh <madhav@thoughtbison.net>
2018-11-19 13:34:36 -0600

    configure.sh refactor: sgx-sdk, reorg
    * This commit refactors configure.sh.
    * Adds support for linux-sgx sdk installation
    * Moves all commands which require root access to
    install-packages.sh
    * Does not redownload and install sources packages
    if folders already exist

--- configure.sh
+++ configure.sh
@@ -1,63 +1,86 @@
 #!/bin/bash
 
-
 BASEDIR=`pwd`
 echo $BASEDIR
 echo "Setting up for vaultdb"
-echo -n Password:
-read -s PASSWORD
-#PASSWORD="" #password"
 
-rm -rf build 
-rm -rf lib
-rm -rf external
+if [ ! -d "build" ]; then
+	mkdir build
+fi
 
-mkdir build && mkdir lib && mkdir external && cd external # make build dir and lib dir
-## IN LIB folder
-echo $PASSWORD | sudo -S apt-get update
-echo $PASSWORD | sudo -S apt-get -y install build-essential autoconf libtool pkg-config cmake git clang libgoogle-perftools-dev
+if [ ! -d "external" ]; then
+	mkdir external
+fi
+
+if [ ! -d "lib" ]; then
+	mkdir lib
+fi
+
 
 
 #gflags installation
-git clone https://github.com/gflags/gflags.git
-  cd gflags
-  mkdir build && cd build
-  cmake -DCMAKE_INSTALL_PREFIX=$BASEDIR/lib ..
-  make -j10
-  make install
 cd $BASEDIR/external
+if [ ! -d "gflags" ]; then
+	git clone https://github.com/gflags/gflags.git
+	cd gflags
+	mkdir build && cd build
+	cmake -DCMAKE_INSTALL_PREFIX=$BASEDIR/lib ..
+	make -j10
+	make install
+	cd $BASEDIR/external
+fi
 
 #grpc installation
-git clone --branch v1.16.1 https://github.com/grpc/grpc.git
-    cd grpc
-    git submodule update --init
-    make HAS_SYSTEM_PROTOBUF=false -j10
-    make prefix=$BASEDIR/lib install
-    cd third_party/protobuf
-    make prefix=$BASEDIR/lib install
-# back in external
 cd $BASEDIR/external
+if [ ! -d "grpc" ]; then
+	git clone --branch v1.16.1 https://github.com/grpc/grpc.git
+	cd grpc
+	git submodule update --init
+	make HAS_SYSTEM_PROTOBUF=false -j10
+	make prefix=$BASEDIR/lib install
+	cd third_party/protobuf
+	make prefix=$BASEDIR/lib install
+	# back in external
+	cd $BASEDIR/external
+fi
 
 #G3Log installation
-git clone --branch 1.3.2 https://github.com/KjellKod/g3log.git
-cd g3log
-cd 3rdParty/gtest
-unzip gtest-1.7.0.zip
-cd ../../
-mkdir build
-cd build
-cmake -DCPACK_PACKAGING_INSTALL_PREFIX=$BASEDIR/lib ..
-make -j10
-make install
+cd $BASEDIR/external
+if [ ! -d "g3log" ]; then
+	git clone --branch 1.3.2 https://github.com/KjellKod/g3log.git
+	cd g3log
+	cd 3rdParty/gtest
+	unzip gtest-1.7.0.zip
+	cd ../../
+	mkdir build
+	cd build
+	cmake -DCPACK_PACKAGING_INSTALL_PREFIX=$BASEDIR/lib ..
+	make -j10
+	make install
+fi
 
 
 #libpqxx installation
 cd $BASEDIR/external
-git clone --branch 6.2.5 https://github.com/jtv/libpqxx.git
-cd libpqxx
-./configure --prefix=$BASEDIR/lib/ --disable-documentation
-make -j10
-make install
+if [ ! -d "libpqxx" ]; then
+	git clone --branch 6.2.5 https://github.com/jtv/libpqxx.git
+	cd libpqxx
+	./configure --prefix=$BASEDIR/lib/ --disable-documentation
+	make -j10
+	make install
+fi
+
+#sgxsdk installation
+cd $BASEDIR/external
+if [ ! -d "linux-sgx" ]; then
+	git clone https://github.com/intel/linux-sgx.git
+	cd linux-sgx
+	./download_prebuilt.sh
+	make -j10 sdk_install_pkg
+	cd ..
+	(echo no; echo $BASEDIR/lib) | ./linux-sgx/linux/installer/bin/sgx_linux_x64_sdk*
+fi
+
 
 cd $BASEDIR
 cd build
---
+++ install-packages.sh
@@ -0,0 +1,5 @@
+echo -n Password:
+read -s PASSWORD
+#PASSWORD="" #password"
+echo $PASSWORD | sudo -S apt-get update
+echo $PASSWORD | sudo -S apt-get -y install build-essential autoconf libtool pkg-config cmake git clang libgoogle-perftools-dev ocaml ocamlbuild
