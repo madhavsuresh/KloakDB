f74718d473695a99e3bf4844dbc537e38fb2c0cb
Madhav Suresh <madhav@thoughtbison.net>
2018-11-24 17:07:32 -0600

    String literal expansion for logging

--- logger/LoggerDefs.h
+++ logger/LoggerDefs.h
@@ -7,14 +7,18 @@
 #include <g3log/g3log.hpp>
 #ifndef LOGDEFS
 #define LOGDEFS
-LEVELS IMPL {INFO.value +1, "RPC-IMPL"};
-LEVELS CLIENT {IMPL.value +1, "RPC-CLIENT"};
-LEVELS STATS {CLIENT.value + 1, "STATS"};
+LEVELS IMPL{INFO.value + 1, "RPC-IMPL"};
+LEVELS CLIENT{IMPL.value + 1, "RPC-CLIENT"};
+LEVELS STATS{CLIENT.value + 1, "STATS"};
 
-#define START_TIMER(timer_name) auto start_##timer_name = std::chrono::high_resolution_clock::now()
-#define END_TIMER(timer_name) auto end_##timer_name = std::chrono::high_resolution_clock::now()
-#define LOG_TIMER(timer_name) std::chrono::duration<double> elapsed_##timer_name = end_##timer_name - start_##timer_name; \
-                              LOG(STATS) << "TIMER ##timer_name: [" << elapsed_##timer_name.count() << "s]"
-#endif //LOGDEFS
+#define START_TIMER(timer_name)                                                \
+  auto start_##timer_name = std::chrono::high_resolution_clock::now()
+#define END_TIMER(timer_name)                                                  \
+  auto end_##timer_name = std::chrono::high_resolution_clock::now()
+#define LOG_TIMER(timer_name)                                                  \
+  std::chrono::duration<double> elapsed_##timer_name =                         \
+      end_##timer_name - start_##timer_name;                                   \
+  LOG(STATS) << "TIMER #timer_name: [" << elapsed_##timer_name.count() << "s]"
+#endif // LOGDEFS
 
-#endif //PROJECT_LOGGERDEFS_H
+#endif // PROJECT_LOGGERDEFS_H
