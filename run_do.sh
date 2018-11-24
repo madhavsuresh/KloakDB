a14bab99d5a6b1e0c7e023e7929e1c6b4feb7212
madhavsuresh <madhav@thoughtbison.net>
2018-11-24 13:49:58 -0600

    run_do.sh should take in host_short flag

--- run_do.sh
+++ run_do.sh
@@ -3,4 +3,4 @@ git pull
 cd build && make -j10
 HOSTNAME=`hostname`
 echo $HOSTNAME
-LD_LIBRARY_PATH=../lib/lib/ ./vaultdb -honest_broker_address="guinea-pig.cs.northwestern.edu:50000" -address="$HOSTNAME.research.northwestern.edu:50000"
+LD_LIBRARY_PATH=../lib/lib/ ./vaultdb -honest_broker_address="guinea-pig.cs.northwestern.edu:50000" -address="$HOSTNAME.research.northwestern.edu:50000" -host_short=$HOSTNAME
