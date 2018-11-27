e3b202889f04b58b2b9382538a3ff0325ad41b71
Madhav Suresh <madhav@thoughtbison.net>
2018-11-26 20:00:52 -0600

    lower range for repartition tables

--- sql/create-random-tables.sql
+++ sql/create-random-tables.sql
@@ -5,15 +5,15 @@ insert into left_deep_joins_4096 (a,b) select floor(random()*10000+1)::int, floo
 
 insert into left_deep_joins_25200 (a,b) select floor(random()*10000+1)::int, floor(random()*100+1)::int from generate_series(1,25200) s(i);
 
-insert into oblivious_partitioning_100 (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,100) s(i);
+insert into oblivious_partitioning_100 (a,b) select floor(random()*1000+1)::int, floor(random()*1000+1)::int from generate_series(1,100) s(i);
 
 
-insert into oblivious_partitioning_1000 (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,1000) s(i);
+insert into oblivious_partitioning_1000 (a,b) select floor(random()*1000+1)::int, floor(random()*1000+1)::int from generate_series(1,1000) s(i);
 
-insert into oblivious_partitioning_10000 (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,10000) s(i);
+insert into oblivious_partitioning_10000 (a,b) select floor(random()*1000+1)::int, floor(random()*1000+1)::int from generate_series(1,10000) s(i);
 
-insert into oblivious_partitioning_100000 (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,100000) s(i);
+insert into oblivious_partitioning_100000 (a,b) select floor(random()*1000+1)::int, floor(random()*1000+1)::int from generate_series(1,100000) s(i);
 
-insert into oblivious_partitioning_1000000 (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,1000000) s(i);
+insert into oblivious_partitioning_1000000 (a,b) select floor(random()*1000+1)::int, floor(random()*1000+1)::int from generate_series(1,1000000) s(i);
 
-insert into oblivious_partitioning_10000000 (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,10000000) s(i);
+insert into oblivious_partitioning_10000000 (a,b) select floor(random()*1000+1)::int, floor(random()*1000+1)::int from generate_series(1,10000000) s(i);
