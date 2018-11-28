48550b7401b6f9b5d376bbaa9cf14860c254d8f4
Madhav Suresh <madhav@thoughtbison.net>
2018-11-27 22:48:06 -0600

    exp4 timing

--- test/experiments/exp4.cpp
+++ test/experiments/exp4.cpp
@@ -5,6 +5,7 @@
 
 
 #include <rpc/HonestBrokerPrivate.h>
+#include <logger/LoggerDefs.h>
 
 void comorbidity(HonestBrokerPrivate *p, std::string dbname,  int gen_level) {
     auto cdiff_cohort_scan =
@@ -17,20 +18,30 @@ void comorbidity(HonestBrokerPrivate *p, std::string dbname,  int gen_level) {
    gen_in["cdiff_cohort_diagnoses"] = tg;
     tg.scan_tables.insert(tg.scan_tables.end(), cdiff_cohort_scan.begin(), cdiff_cohort_scan.end());
     p->SetControlFlowColName("major_icd9");
+    START_TIMER(generalize);
     auto gen_out = p->Generalize(gen_in, gen_level);
-    auto cdiff_cohort_repart = p->Repartition(cdiff_cohort_scan);
+    END_AND_LOG_EXEC_TIMER(generalize);
+    START_TIMER(repartition);
+    auto cdiff_cohort_repart = p->Repartition(gen_out["cdiff_cohort_diagnoses"]);
+    END_AND_LOG_EXEC_TIMER(repartition);
 
     GroupByDef gbd;
     gbd.set_col_name("major_icd9");
     gbd.set_type(GroupByDef_GroupByType_COUNT);
+    START_TIMER(aggregate);
     auto agg_out = p->Aggregate(cdiff_cohort_repart, gbd, true);
+    END_AND_LOG_EXEC_TIMER(aggregate);
 
     p->SetControlFlowColName("count");
+    START_TIMER(repartition2);
     auto cnt_repartition = p->Repartition(agg_out);
+    END_AND_LOG_EXEC_TIMER(repartition2);
     SortDef sort;
     sort.set_colname("count");
     sort.set_ascending(false);
+    START_TIMER(sort);
     auto sorted = p->Sort(cnt_repartition, sort, true);
+    END_AND_LOG_EXEC_TIMER(sort);
 }
 
 void exp4(HonestBrokerPrivate *p) {
