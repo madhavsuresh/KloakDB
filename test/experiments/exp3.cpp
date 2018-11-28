94782e5028ca1882bebc5f17814e91db7b9a8642
Madhav Suresh <madhav@thoughtbison.net>
2018-11-28 10:17:50 -0600

    pretty print

--- test/experiments/exp3.cpp
+++ test/experiments/exp3.cpp
@@ -14,12 +14,13 @@ DEFINE_int32(exp3_datasize, 100, "exp3 data size");
  */
 
 void exp3(HonestBrokerPrivate *p) {
-  LOG(EXP3_STAT) << "STARTING EXP3 WITH DATA SIZE:[" << FLAGS_exp3_datasize;
+  LOG(EXP3_STAT) << "=============STARTING EXP3 WITH DATA SIZE:[" << FLAGS_exp3_datasize << "] ============";
+  p->SetControlFlowColName("b");
   auto scan = p->ClusterDBMSQuery("dbname=vaultdb_",
                                   "SELECT * from oblivious_partitioning_" +
                                       std::to_string(FLAGS_exp3_datasize));
-  p->SetControlFlowColName("b");
   START_TIMER(repartition_outer);
   p->Repartition(scan);
   END_AND_LOG_EXP3_STAT_TIMER(repartition_outer);
+  LOG(EXP3_STAT) << "============ENDING EXP3 WITH DATA SIZE:[" << FLAGS_exp3_datasize << "]=========";
 }
