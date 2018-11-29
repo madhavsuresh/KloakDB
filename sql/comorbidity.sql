9c16e52a3d8c06397f0e37bc87c8d77024a9cdaa
Madhav Suresh <madhav@thoughtbison.net>
2018-11-29 12:00:01 -0600

    cdiff cohort create scripts

--- sql/comorbidity.sql
+++ sql/comorbidity.sql
@@ -1,8 +1,7 @@
-DROP TABLE IF EXISTS cdiff_cohort_diagnoses;
-CREATE TABLE cdiff_cohort_diagnoses AS SELECT * FROM diagnoses WHERE patient_id IN (SELECT * FROM cdiff_cohort); 
 
 SELECT d.major_icd9, count(*) as cnt 
- FROM cdiff_cohort_diagnoses d                                                                                         GROUP BY d.major_icd9                                                        
+ FROM cdiff_cohort_diagnoses d
+ GROUP BY d.major_icd9                                                        
  ORDER BY count(*) DESC 
  LIMIT 10;
 
