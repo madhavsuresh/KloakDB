a3383e1d26a96b38410fafae643a5303baa67c1f
Madhav Suresh <madhav@thoughtbison.net>
2018-11-29 17:17:21 -0600

    mixed, multi column repartitioning

--- test/operators/repart_test.cpp
+++ test/operators/repart_test.cpp
@@ -52,3 +52,19 @@ TEST_F(repart_test, string_multi_column) {
   ASSERT_NE(hash_to_host(cf, 4, get_tuple(1, t), t), hash_to_host(cf, 4, get_tuple(3, t),t));
   ASSERT_EQ(hash_to_host(cf, 4, get_tuple(3, t), t), hash_to_host(cf, 4, get_tuple(4, t),t));
 }
+
+TEST_F(repart_test, mixed_column) {
+
+  ::vaultdb::ControlFlowColumn cf;
+  cf.add_cf_name_strings("a");
+  cf.add_cf_name_strings("b");
+  cf.add_cf_name_strings("d");
+  query("CREATE TABLE test (a VARCHAR, b VARCHAR, c VARCHAR, d INT)", dbname);
+  query("INSERT INTO test (a,b,c,d) VALUES ('1a','2b','3c', 1), ('1a','2b','4c', 1), ('1a','2b','5d', 2), ('1a','4z','5e', 10), ('1a','4z','6hh', 10)", dbname);
+  table_t * t = get_table("SELECT * FROM test", dbname);
+
+  ASSERT_EQ(hash_to_host(cf, 4, get_tuple(0, t), t), hash_to_host(cf, 4, get_tuple(1, t),t));
+  ASSERT_NE(hash_to_host(cf, 4, get_tuple(1, t), t), hash_to_host(cf, 4, get_tuple(2, t),t));
+  ASSERT_NE(hash_to_host(cf, 4, get_tuple(1, t), t), hash_to_host(cf, 4, get_tuple(3, t),t));
+  ASSERT_EQ(hash_to_host(cf, 4, get_tuple(3, t), t), hash_to_host(cf, 4, get_tuple(4, t),t));
+}
