111d40dba94d09dafe7e765fad9e6e2ecd1d64de
madhavsuresh <madhav@thoughtbison.net>
2018-11-30 02:50:52 +0000

    update get_count_star to print

--- sql/get_count_star.sql
+++ sql/get_count_star.sql
@@ -1,3 +1,6 @@
+\echo 'medications'
 select year, count(*) from medications group by year;
+\echo 'diagnoses'
 select year, count(*) from diagnoses group by year;
+\echo 'vitals'
 select year, count(*) from vitals group by year;
