aacb1df7f3da7eaf298baa36e5ded2f707765ca4
madhavsuresh <madhav@thoughtbison.net>
2018-12-01 18:13:11 +0000

    use vaultdb03

--- scripts/run_hb.sh
+++ scripts/run_hb.sh
@@ -4,7 +4,7 @@ killall -9 vaultdb
 git pull 
 cd ../build && make -j10 vaultdb
 echo "Starting vaultdb"
- LD_LIBRARY_PATH=../lib/lib/ ./vaultdb -address="vaultdb04.research.northwestern.edu:50000" -honest_broker=true  -host_short='v4' $1&
+ LD_LIBRARY_PATH=../lib/lib/ ./vaultdb -address="vaultdb03.research.northwestern.edu:50000" -honest_broker=true  -host_short='v4' $1&
 ssh mas384@vaultdb01.research.northwestern.edu 'cd vaultdb_operators && ./run_do.sh' &
 ssh mas384@vaultdb02.research.northwestern.edu 'cd vaultdb_operators && ./run_do.sh' &
 #ssh mas384@vaultdb03.research.northwestern.edu 'cd vaultdb_operators && ./run_do.sh' &
