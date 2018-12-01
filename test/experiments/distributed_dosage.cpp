b0267c033f003f9e207700cbf854ca07bcb45b59
Madhav Suresh <madhav@thoughtbison.net>
2018-12-01 12:27:01 -0600

    no filter push down

--- test/experiments/distributed_dosage.cpp
+++ test/experiments/distributed_dosage.cpp
@@ -85,9 +85,9 @@ void dosage_k(HonestBrokerPrivate *p, std::string dbname,
   LOG(EXP7_DOS) << "STARTING DOSAGE STUDY K-ANONYMOUS";
   START_TIMER(dosage_study_k);
   auto diag_scan = p->ClusterDBMSQuery("dbname=" + dbname,
-                                       "SELECT * from " + diag + year_append + " AND icd9 LIKE '997%'");
+                                       "SELECT * from " + diag + year_append); //;+ " AND icd9 LIKE '997%'");
   auto med_scan = p->ClusterDBMSQuery("dbname=" + dbname,
-                                      "SELECT * from " + meds + year_append + " AND medication LIKE 'ASPIRIN%' AND dosage = '325 MG'");
+                                      "SELECT * from " + meds + year_append + "); // AND medication LIKE 'ASPIRIN%' AND dosage = '325 MG'");
   // auto to_join = zip_join_tables(diag_scan, med_scan);
   to_gen_t meds_gen;
   meds_gen.column = "patient_id";
