60a93d1d2c007ad2260816c580dcb0aa323f9167
Madhav Suresh <madhav@thoughtbison.net>
2018-12-01 11:48:12 -0600

    count star should only be for 2008

--- test/experiments/distributed_dosage.cpp
+++ test/experiments/distributed_dosage.cpp
@@ -85,7 +85,7 @@ void dosage_k(HonestBrokerPrivate *p, std::string dbname,
   LOG(EXP7_DOS) << "STARTING DOSAGE STUDY K-ANONYMOUS";
   START_TIMER(dosage_study_k);
   auto diag_scan = p->ClusterDBMSQuery("dbname=" + dbname,
-                                       "SELECT * from " + diag + year_append + " AND icd9 LIKE '997%'");
+                                       "SELECT * from " + diag + year_append + " AND icd9 LIKE '997%' AND dosage = '325 MG'");
   auto med_scan = p->ClusterDBMSQuery("dbname=" + dbname,
                                       "SELECT * from " + meds + year_append + " AND medication LIKE 'ASPIRIN%'");
   // auto to_join = zip_join_tables(diag_scan, med_scan);
