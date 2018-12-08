af03cfd555142e8ee78dfb07daa23532c7fcd68f
Madhav Suresh <madhav@thoughtbison.net>
2018-12-07 20:25:04 -0600

    small view gen needs to be smaller for oblivious

--- sql/small_view_gen.sql
+++ sql/small_view_gen.sql
@@ -1,7 +1,14 @@
-select setseed(.0001);
+select setseed(.0002);
 DROP TABLE IF EXISTS dem_250, diag_250, vitals_250, medications_250;
 
 SELECT * INTO dem_250 FROM (SELECT * FROM demographics ORDER BY random()) t LIMIT 250;
 SELECT * INTO diag_250 FROM (SELECT * FROM diagnoses ORDER BY random()) t LIMIT 250;
 SELECT * INTO vitals_250 FROM (SELECT * FROM vitals ORDER BY random()) t LIMIT 250;
 SELECT * INTO medications_250 FROM (SELECT * FROM medications ORDER BY random()) t LIMIT 250;
+
+DROP TABLE IF EXISTS dem_125, diag_125, vitals_125, medications_125;
+
+SELECT * INTO dem_125 FROM (SELECT * FROM demographics ORDER BY random()) t LIMIT 125;
+SELECT * INTO diag_125 FROM (SELECT * FROM diagnoses ORDER BY random()) t LIMIT 125;
+SELECT * INTO vitals_125 FROM (SELECT * FROM vitals ORDER BY random()) t LIMIT 125;
+SELECT * INTO medications_125 FROM (SELECT * FROM medications ORDER BY random()) t LIMIT 125;
