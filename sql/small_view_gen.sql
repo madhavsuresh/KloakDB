0318a43bc84dd4160a7f12404d28ea6865eaa6e1
Madhav Suresh <madhav@thoughtbison.net>
2018-12-07 20:37:30 -0600

    not null in vitals

--- sql/small_view_gen.sql
+++ sql/small_view_gen.sql
@@ -3,12 +3,12 @@ DROP TABLE IF EXISTS dem_250, diag_250, vitals_250, medications_250;
 
 SELECT * INTO dem_250 FROM (SELECT * FROM demographics ORDER BY random()) t LIMIT 250;
 SELECT * INTO diag_250 FROM (SELECT * FROM diagnoses ORDER BY random()) t LIMIT 250;
-SELECT * INTO vitals_250 FROM (SELECT * FROM vitals ORDER BY random()) t LIMIT 250;
+SELECT * INTO vitals_250 FROM (SELECT * FROM vitals WHERE pulse is NOT NULL ORDER BY random()) t LIMIT 250;
 SELECT * INTO medications_250 FROM (SELECT * FROM medications ORDER BY random()) t LIMIT 250;
 
 DROP TABLE IF EXISTS dem_125, diag_125, vitals_125, medications_125;
 
 SELECT * INTO dem_125 FROM (SELECT * FROM demographics ORDER BY random()) t LIMIT 125;
 SELECT * INTO diag_125 FROM (SELECT * FROM diagnoses ORDER BY random()) t LIMIT 125;
-SELECT * INTO vitals_125 FROM (SELECT * FROM vitals ORDER BY random()) t LIMIT 125;
+SELECT * INTO vitals_125 FROM (SELECT * FROM vitals WHERE pulse is NOT NULL ORDER BY random()) t LIMIT 125;
 SELECT * INTO medications_125 FROM (SELECT * FROM medications ORDER BY random()) t LIMIT 125;
