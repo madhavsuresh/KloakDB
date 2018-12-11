c56cb956edd6323d2da694b2cdf46ade55d02b8f
madhavsuresh <madhav@thoughtbison.net>
2018-12-10 23:04:32 -0600

    typos in new views gen

--- sql/exp.sql
+++ sql/exp.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS dem_ex, diag_ex, vit_ex, meds_ex, hd_cohort;
-SELECT * INTO hd_cohort FROM (SELECT DISTINCT patient_id from diagnoses WHERE year=2008 and icd9 LIKE '414%');
-SELECT * INTO dem_ex FROM (SELECT * FROM demographics where patient_id in (SELECT * from hd_cohort) t;
-SELECT * INTO vit_ex FROM (SELECT * FROM vitals WHERE year=2008 AND pulse IS NOT NULL AND patient_id in (SELECT * from hd_cohort) t;
+SELECT * INTO hd_cohort FROM (SELECT DISTINCT patient_id from diagnoses WHERE year=2008 and icd9 LIKE '414%') t;
+SELECT * INTO dem_ex FROM (SELECT * FROM demographics where patient_id in (SELECT * from hd_cohort)) t;
+SELECT * INTO vit_ex FROM (SELECT * FROM vitals WHERE year=2008 AND pulse IS NOT NULL AND patient_id in (SELECT * from hd_cohort)) t;
 SELECT * INTO meds_ex FROM (SELECT * FROM medications WHERE year=2008 AND patient_id in (SELECT * from hd_cohort)) t; ---AND medication ILIKE '%aspirin%') t;
