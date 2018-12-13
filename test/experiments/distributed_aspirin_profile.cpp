2ff2fb437642b18f1c5d58e4515c50a994cb72c8
Madhav Suresh <madhav@thoughtbison.net>
2018-12-12 18:46:10 -0600

    genzip, set discards to -1

--- test/experiments/distributed_aspirin_profile.cpp
+++ test/experiments/distributed_aspirin_profile.cpp
@@ -469,7 +469,6 @@ void aspirin_profile_gen(HonestBrokerPrivate *p, std::string database,
   auto to_join1 = zip_join_tables(vitals_repart, diagnoses_repart);
   auto out_vd_join = p->Join(to_join1, jd_vd, false);
   END_AND_LOG_EXP7_ASP_STAT_TIMER(join_one, "full");
-  /*
   START_TIMER(join_two);
   JoinDef jd_pm2;
   jd_pm2.set_l_col_name("patient_id");
@@ -486,6 +485,5 @@ void aspirin_profile_gen(HonestBrokerPrivate *p, std::string database,
   auto to_join2 = zip_join_tables(out_vd_join, meds_repart);
   auto out_pm_join = p->Join(to_join2, jd_pm2, sgx);
   END_AND_LOG_EXP7_ASP_STAT_TIMER(join_two, "full");
-   */
   END_AND_LOG_EXP7_ASP_STAT_TIMER(aspirin_profile_full, "full");
 }
