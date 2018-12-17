39c0f0e24f0a2a703e9b076d4ba4181d3168b638
Madhav Suresh <madhav@thoughtbison.net>
2018-12-17 14:45:23 -0600

    char internal gen bug fix

--- test/experiments/distributed_comorb.cpp
+++ test/experiments/distributed_comorb.cpp
@@ -81,7 +81,7 @@ void comorbidity_keq5(HonestBrokerPrivate *p, std::string dbname, std::string ye
   auto gen_out = p->Generalize(gen_in, 5);
   END_AND_LOG_EXEC_TIMER(generalize);
   START_TIMER(repartition);
-  auto cdiff_cohort_repart = p->Repartition(cdiff_cohort_scan);
+  auto cdiff_cohort_repart = p->Repartition(gen_out["cdiff_cohort_diagnoses"]);
   END_AND_LOG_EXEC_TIMER(repartition);
 
   GroupByDef gbd;
