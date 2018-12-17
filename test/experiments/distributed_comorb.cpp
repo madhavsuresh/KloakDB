b401b8aaa27f39b07943fcd5047e4d1a9a5debbf
Madhav Suresh <madhav@thoughtbison.net>
2018-12-17 09:10:52 -0600

    secure groupbydef for comorb

--- test/experiments/distributed_comorb.cpp
+++ test/experiments/distributed_comorb.cpp
@@ -81,10 +81,11 @@ void comorbidity_keq5(HonestBrokerPrivate *p, std::string dbname, std::string ye
   END_AND_LOG_EXEC_TIMER(repartition);
 
   GroupByDef gbd;
+  gbd.set_secure(true);
   gbd.set_col_name("major_icd9");
   gbd.set_type(GroupByDef_GroupByType_COUNT);
   START_TIMER(aggregate);
-  auto agg_out = p->Aggregate(cdiff_cohort_repart, gbd, false);
+  auto agg_out = p->Aggregate(cdiff_cohort_repart, gbd, true);
   END_AND_LOG_EXEC_TIMER(aggregate);
 
   p->SetControlFlowColName("count");
@@ -95,7 +96,7 @@ void comorbidity_keq5(HonestBrokerPrivate *p, std::string dbname, std::string ye
   sort.set_colname("count");
   sort.set_ascending(false);
   START_TIMER(sort);
-  auto sorted = p->Sort(cnt_repartition, sort, false);
+  auto sorted = p->Sort(cnt_repartition, sort, true);
   END_AND_LOG_EXEC_TIMER(sort);
   END_AND_LOG_EXP7_COM_STAT_TIMER(comorbidity_kanon_full, "insgx");
 }
