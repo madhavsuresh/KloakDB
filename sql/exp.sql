80f8dbf805683abbfada0e1ff830d072f3700b02
madhavsuresh <madhav@thoughtbison.net>
2018-12-20 01:05:04 +0000

    fdw

--- sql/exp.sql
+++ sql/exp.sql
@@ -12,6 +12,7 @@ SELECT * INTO vit_ex_local FROM (SELECT * FROM vitals WHERE pulse IS NOT NULL AN
 SELECT * INTO meds_ex_local FROM (SELECT * FROM medications WHERE (year=2008) AND patient_id in (SELECT * from hd_cohort_local)) t; ---AND medication ILIKE '%aspirin%') t;
 
 -- FDW, used for FDW queries
-SELECT * INTO dem_ex FROM (SELECT * FROM demographics where patient_id in (SELECT * from hd_cohort)) t;
-SELECT * INTO vit_ex FROM (SELECT * FROM vitals WHERE pulse IS NOT NULL AND (year=2008) AND patient_id in (SELECT * from hd_cohort)) t;
-SELECT * INTO meds_ex FROM (SELECT * FROM medications WHERE (year=2008) AND patient_id in (SELECT * from hd_cohort)) t; ---AND medication ILIKE '%aspirin%') t;
+DROP TABLE IF EXISTS dem_ex_fdw, vit_ex_fdw, meds_ex_fdw;
+SELECT * INTO dem_ex_fdw FROM (SELECT * FROM demographics where patient_id in (SELECT * from hd_cohort)) t;
+SELECT * INTO vit_ex_fdw FROM (SELECT * FROM vitals WHERE pulse IS NOT NULL AND (year=2008) AND patient_id in (SELECT * from hd_cohort)) t;
+SELECT * INTO meds_ex_fdw FROM (SELECT * FROM medications WHERE (year=2008) AND patient_id in (SELECT * from hd_cohort)) t; ---AND medication ILIKE '%aspirin%') t;
