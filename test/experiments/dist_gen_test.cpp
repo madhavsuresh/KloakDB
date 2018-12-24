307adfc578d91ffc1fbf80d294aeb1d2e4049127
Madhav Suresh <madhav@thoughtbison.net>
2018-12-24 12:56:04 -0600

    gen test

---
+++ test/experiments/dist_gen_test.cpp
@@ -0,0 +1,45 @@
+//
+// Created by madhav on 12/23/18.
+//
+
+#include "dist_gen_test.h"
+#include <VaultDB.h>
+
+void gen_test_rand_table(HonestBrokerPrivate *p, std::string dbname,
+                         std::string table1, std::string table2, int k,
+                         bool in_sgx) {
+  std::string column = "b";
+  auto gen_scan1 =
+      p->ClusterDBMSQuery("dbname=" + dbname, "SELECT * FROM " + table1);
+  auto gen_scan2 =
+      p->ClusterDBMSQuery("dbname=" + dbname, "SELECT * FROM " + table2);
+
+  unordered_map<table_name, to_gen_t> gen_in;
+  to_gen_t scan1;
+  scan1.column = column;
+  scan1.dbname = dbname;
+  scan1.scan_tables.insert(scan1.scan_tables.end(), gen_scan1.begin(),
+                           gen_scan1.end());
+  gen_in[table1] = scan1;
+
+  to_gen_t scan2;
+  scan2.column = column;
+  scan2.dbname = dbname;
+  scan2.scan_tables.insert(scan2.scan_tables.end(), gen_scan2.begin(),
+                           gen_scan2.end());
+  gen_in[table2] = scan2;
+  p->SetControlFlowColName(column);
+  auto gen_zipped_map = p->Generalize(gen_in, k);
+  auto table1_repart = p->Repartition(gen_zipped_map[table1]);
+  auto table2_repart = p->Repartition(gen_zipped_map[table2]);
+  JoinDef jd;
+  jd.set_l_col_name(column);
+  jd.set_r_col_name(column);
+  jd.set_project_len(1);
+  auto join_project = jd.add_project_list();
+  join_project->set_colname(column);
+  join_project->set_col_no(JoinColID_RelationSide_LEFT);
+
+  auto to_join = zip_join_tables(table1_repart, table2_repart);
+  auto output_join = p->Join(to_join, jd, in_sgx);
+}
---
+++ test/experiments/dist_gen_test.h
@@ -0,0 +1,13 @@
+//
+// Created by madhav on 12/23/18.
+//
+
+#ifndef PROJECT_DIST_GEN_TEST_H
+#define PROJECT_DIST_GEN_TEST_H
+#include <rpc/HonestBrokerPrivate.h>
+
+void gen_test_rand_table(HonestBrokerPrivate *p, std::string dbname,
+                         std::string table1, std::string table2, int k,
+                         bool in_sgx);
+
+#endif // PROJECT_DIST_GEN_TEST_H
