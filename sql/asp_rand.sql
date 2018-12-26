9d78ec4ece79abe898d8bb5cc5d50c39ee3a1431
Madhav Suresh <madhav@thoughtbison.net>
2018-12-26 12:50:48 -0600

    add 8th table

--- sql/asp_rand.sql
+++ sql/asp_rand.sql
@@ -1,22 +1,25 @@
 ---RANDOM 25 for ASPIRIN Profile Oblivious Queries
 select setseed(.0001);
-DROP TABLE IF EXISTS hd_cohort_dist_25_1, meds_ex_25_1, vit_ex_25_1, dem_ex_25_1;
+DROP TABLE IF EXISTS hd_cohort_dist_25_1;
 SELECT * INTO hd_cohort_dist_25_1 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
 
-DROP TABLE IF EXISTS hd_cohort_dist_25_2, meds_ex_25_2, vit_ex_25_2, dem_ex_25_2;
+DROP TABLE IF EXISTS hd_cohort_dist_25_2;
 SELECT * INTO hd_cohort_dist_25_2 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
 
-DROP TABLE IF EXISTS hd_cohort_dist_25_3, meds_ex_25_3, vit_ex_25_3, dem_ex_25_3;
+DROP TABLE IF EXISTS hd_cohort_dist_25_3;
 SELECT * INTO hd_cohort_dist_25_3 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
 
-DROP TABLE IF EXISTS hd_cohort_dist_25_4, meds_ex_25_4, vit_ex_25_4, dem_ex_25_4;
+DROP TABLE IF EXISTS hd_cohort_dist_25_4;
 SELECT * INTO hd_cohort_dist_25_4 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
 
-DROP TABLE IF EXISTS hd_cohort_dist_25_5, meds_ex_25_5, vit_ex_25_5, dem_ex_25_5;
+DROP TABLE IF EXISTS hd_cohort_dist_25_5;
 SELECT * INTO hd_cohort_dist_25_5 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
 
-DROP TABLE IF EXISTS hd_cohort_dist_25_6, meds_ex_25_6, vit_ex_25_6, dem_ex_25_6;
+DROP TABLE IF EXISTS hd_cohort_dist_25_6;
 SELECT * INTO hd_cohort_dist_25_6 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
 
-DROP TABLE IF EXISTS hd_cohort_dist_25_7, meds_ex_25_7, vit_ex_25_7, dem_ex_25_7;
+DROP TABLE IF EXISTS hd_cohort_dist_25_7;
 SELECT * INTO hd_cohort_dist_25_7 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
+
+DROP TABLE IF EXISTS hd_cohort_dist_25_8;
+SELECT * INTO hd_cohort_dist_25_8 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
