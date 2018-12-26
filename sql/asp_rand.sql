b93a3a9eae1d40daa85438afd88a4b85e8ee0017
Madhav Suresh <madhav@thoughtbison.net>
2018-12-26 10:47:04 -0600

    new asp_rand.sql

--- sql/asp_rand.sql
+++ sql/asp_rand.sql
@@ -1,36 +1,22 @@
 ---RANDOM 25 for ASPIRIN Profile Oblivious Queries
 select setseed(.0001);
+DROP TABLE IF EXISTS hd_cohort_dist_25_1, meds_ex_25_1, vit_ex_25_1, dem_ex_25_1;
 SELECT * INTO hd_cohort_dist_25_1 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
-SELECT * INTO meds_ex_25_1 FROM (SELECT * FROM meds_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO vit_ex_25_1 FROM (SELECT * FROM vit_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO dem_ex_25_1 FROM (SELECT * FROM dem_ex ORDER BY random()) t LIMIT 25;
 
+DROP TABLE IF EXISTS hd_cohort_dist_25_2, meds_ex_25_2, vit_ex_25_2, dem_ex_25_2;
 SELECT * INTO hd_cohort_dist_25_2 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
-SELECT * INTO meds_ex_25_2 FROM (SELECT * FROM meds_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO vit_ex_25_2 FROM (SELECT * FROM vit_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO dem_ex_25_2 FROM (SELECT * FROM dem_ex ORDER BY random()) t LIMIT 25;
 
+DROP TABLE IF EXISTS hd_cohort_dist_25_3, meds_ex_25_3, vit_ex_25_3, dem_ex_25_3;
 SELECT * INTO hd_cohort_dist_25_3 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
-SELECT * INTO meds_ex_25_3 FROM (SELECT * FROM meds_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO vit_ex_25_3 FROM (SELECT * FROM vit_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO dem_ex_25_3 FROM (SELECT * FROM dem_ex ORDER BY random()) t LIMIT 25;
 
+DROP TABLE IF EXISTS hd_cohort_dist_25_4, meds_ex_25_4, vit_ex_25_4, dem_ex_25_4;
 SELECT * INTO hd_cohort_dist_25_4 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
-SELECT * INTO meds_ex_25_4 FROM (SELECT * FROM meds_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO vit_ex_25_4 FROM (SELECT * FROM vit_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO dem_ex_25_4 FROM (SELECT * FROM dem_ex ORDER BY random()) t LIMIT 25;
 
+DROP TABLE IF EXISTS hd_cohort_dist_25_5, meds_ex_25_5, vit_ex_25_5, dem_ex_25_5;
 SELECT * INTO hd_cohort_dist_25_5 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
-SELECT * INTO meds_ex_25_5 FROM (SELECT * FROM meds_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO vit_ex_25_5 FROM (SELECT * FROM vit_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO dem_ex_25_5 FROM (SELECT * FROM dem_ex ORDER BY random()) t LIMIT 25;
 
+DROP TABLE IF EXISTS hd_cohort_dist_25_6, meds_ex_25_6, vit_ex_25_6, dem_ex_25_6;
 SELECT * INTO hd_cohort_dist_25_6 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
-SELECT * INTO meds_ex_25_6 FROM (SELECT * FROM meds_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO vit_ex_25_6 FROM (SELECT * FROM vit_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO dem_ex_25_6 FROM (SELECT * FROM dem_ex ORDER BY random()) t LIMIT 25;
 
+DROP TABLE IF EXISTS hd_cohort_dist_25_7, meds_ex_25_7, vit_ex_25_7, dem_ex_25_7;
 SELECT * INTO hd_cohort_dist_25_7 FROM (SELECT * FROM hd_cohort_dist ORDER BY random()) t LIMIT 25;
-SELECT * INTO meds_ex_25_7 FROM (SELECT * FROM meds_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO vit_ex_25_7 FROM (SELECT * FROM vit_ex ORDER BY random()) t LIMIT 25;
-SELECT * INTO dem_ex_25_7 FROM (SELECT * FROM dem_ex ORDER BY random()) t LIMIT 25;
