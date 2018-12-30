76281e398ab2fb6040d3bdcc42d6b8808a2e5870
Madhav Suresh <madhav@thoughtbison.net>
2018-12-30 13:01:31 -0600

    what is happening

--- VaultDB.cpp
+++ VaultDB.cpp
@@ -166,6 +166,7 @@ int main(int argc, char **argv) {
     auto enclave = get_enclave();
     p->WaitForAllHosts();
     p->RegisterPeerHosts();
+    this_thread::sleep_for(chrono::milliseconds(1000));
     // dosage_study(p);
     // comorbidity(p);
     // aspirin_profile(p);
