aef9d96f3a46fb5cdba25e0339ede090bf0ab0db
Madhav Suresh <madhav@thoughtbison.net>
2018-12-30 12:11:18 -0600

    exp5 random tables

---
+++ sql/create-exp5-rand.sh
@@ -0,0 +1,1 @@
+psql healthlnk -f exp5.sql
---
+++ sql/exp5.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS left_deep_joins_512_a, left_deep_joins_512_b, left_deep_joins_512_c, left_deep_joins_512_d;
+
+CREATE TABLE left_deep_joins_512_a (
+	a INT,
+	b INT
+);
+
+CREATE TABLE left_deep_joins_512_b (
+	a INT,
+	b INT
+);
+
+CREATE TABLE left_deep_joins_512_c (
+	a INT,
+	b INT
+);
+
+CREATE TABLE left_deep_joins_512_d (
+	a INT,
+	b INT
+);
+
+insert into left_deep_joins_512_a (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
+insert into left_deep_joins_512_b (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
+insert into left_deep_joins_512_c (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
+insert into left_deep_joins_512_d (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
