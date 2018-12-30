5ef55a4b8b1aa3ad751125a50735eca3e2734e4b
madhavsuresh <madhav@thoughtbison.net>
2018-12-30 12:32:13 -0600

    add fifth table

--- sql/exp5.sql
+++ sql/exp5.sql
@@ -1,5 +1,5 @@
-select setseed(.0001);
-DROP TABLE IF EXISTS left_deep_joins_512_a, left_deep_joins_512_b, left_deep_joins_512_c, left_deep_joins_512_d;
+select setseed(.0003);
+DROP TABLE IF EXISTS left_deep_joins_512_a, left_deep_joins_512_b, left_deep_joins_512_c, left_deep_joins_512_d,left_deep_joins_512_e;
 
 CREATE TABLE left_deep_joins_512_a (
 	a INT,
@@ -20,8 +20,13 @@ CREATE TABLE left_deep_joins_512_d (
 	a INT,
 	b INT
 );
+CREATE TABLE left_deep_joins_512_e (
+	a INT,
+	b INT
+);
 
 insert into left_deep_joins_512_a (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
 insert into left_deep_joins_512_b (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
 insert into left_deep_joins_512_c (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
 insert into left_deep_joins_512_d (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
+insert into left_deep_joins_512_e (a,b) select floor(random()*10000+1)::int, floor(random()*10000+1)::int from generate_series(1,512) s(i);
