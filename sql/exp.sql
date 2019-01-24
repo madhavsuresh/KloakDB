48e661189deebee02f301c55f6ee32f1f483089f
madhavsuresh <madhav@thoughtbison.net>
2019-01-24 19:21:16 +0000

    meds_ex_filter for dosage study

--- sql/exp.sql
+++ sql/exp.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS dem_ex, diag_ex, vit_ex, meds_ex, hd_cohort;
+DROP TABLE IF EXISTS dem_ex, diag_ex, vit_ex, meds_ex, hd_cohort, meds_ex_filter;
 SELECT * INTO hd_cohort FROM (SELECT DISTINCT patient_id from diagnoses WHERE icd9 LIKE '414%' AND (year=2008)) t;
 SELECT * INTO dem_ex FROM (SELECT * FROM demographics where patient_id in (SELECT * from hd_cohort_dist)) t;
 SELECT * INTO vit_ex FROM (SELECT * FROM vitals WHERE pulse IS NOT NULL AND (year=2008) AND patient_id in (SELECT * from hd_cohort_dist)) t;
 SELECT * INTO meds_ex FROM (SELECT * FROM medications WHERE (year=2008) AND patient_id in (SELECT * from hd_cohort_dist)) t; ---AND medication ILIKE '%aspirin%') t;
+SELECT * INTO meds_ex_filter FROM (SELECT * FROM meds_ex WHERE medication ILIKE '%aspirin%' AND dosage LIKE '%325%') t; ---AND medication ILIKE '%aspirin%') t;
 
 --- LOCAL, used for semi-join optimization
 DROP TABLE IF EXISTS dem_ex_local, diag_ex_local, vit_ex_local, meds_ex_local, hd_cohort_local;
